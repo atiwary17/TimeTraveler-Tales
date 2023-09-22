67b06b0e6c619304c4fff51c112980879b7322d1
atiwary17 <142500677+atiwary17@users.noreply.github.com>
2023-09-22 15:50:11 +0400

    Update bundle.js

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -181,7 +181,7 @@
           , b = document.getElementsByClassName("winner")[0]
           , f = document.querySelector("footer")
           , p = document.getElementsByClassName("icons-authors")[0]
-          , y = new Audio("/time-machine2/static/roulette-wheel.mp3")
+          , y = new Audio("/static/roulette-wheel.mp3")
           , I = h.getContext("2d")
           , M = h.width / 2
           , C = h.height / 2
