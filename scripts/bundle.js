13aa71fa3b5a541a5307069ecf889622efc69fb4
atiwary17 <142500677+atiwary17@users.noreply.github.com>
2023-09-29 22:00:05 +0400

    Update bundle.js

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -174,11 +174,11 @@
     const videoURL = `${baseURL}${videoName}`;
     const videoURL3 = `https://www.youtube.com/embed/ZyiFeREyLYo`;
 
-    const videoPuzzleURLs = {
-        'video1.mp4': 'https://puzzel.org/quiz/play?p=-NfNljz4o-HKgJ6ULm8T',
-        'video2.mp4': 'https://example.com/puzzle2',
-        // Add more video puzzle URLs as needed
-    };
+    const videoURLs = {
+    video1: 'https://puzzel.org/quiz/play?p=-NfNljz4o-HKgJ6ULm8T',
+    video2: 'https://example.com/video2',
+    // Add more video URLs as needed
+};
 
     // Open the video in a new tab
     const newTab = window.open('', '_blank');
