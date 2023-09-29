57d0f67ca01eff05fca736a5a3ac99a77ac3fde8
atiwary17 <142500677+atiwary17@users.noreply.github.com>
2023-09-29 22:32:23 +0400

    Update bundle.js

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -172,7 +172,7 @@
     // Define the URL to the video in your GitHub repository
     const baseURL = 'https://atiwary17.github.io/time-machine2/videos/';
     const videoURL = `${baseURL}${videoName}`;
-    const videoURL3 = `https://www.youtube.com/watch?v=rFOwpX7y-rw`;
+    const videoURL3 = `https://www.youtube.com/watch?v=RLzC55ai0eo`;
 
     const videoPuzzleURLs = {
         video1: 'https://puzzel.org/quiz/play?p=-NfNljz4o-HKgJ6ULm8T',
